c06c6315fb11105f9edc94f80ad23594a7e819f6
YashMengji <yashmengji2172005@gmail.com>
2024-09-27 22:00:06 +0530

    nodemon update

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json()); 
 app.use(express.urlencoded({extended: true})); 
 app.use(cookieParser());
-
+  
 app.get("/", (req, res) => {
   res.send("Home page");
 })
