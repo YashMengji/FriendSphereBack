5d50998fadc8e7fa925224fdd0005750bbfb78e4
YashMengji <yashmengji2172005@gmail.com>
2025-01-03 09:25:40 +0530

    added post Routes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -4,9 +4,9 @@ const { getAllPosts, getSinglePost, createComment, updateComment, deleteComment 
 const router = express.Router();
 
 router.get("/", getAllPosts);
-router.get("/posts/:id", getSinglePost);
-router.post("/posts/:id/comments", createComment);
-router.put("/posts/:postId/comments/:commentId", updateComment);
-router.delete("/posts/:postId/comments/:commentId", deleteComment);
+router.get("/:id", getSinglePost);
+router.post("/:id/comments", createComment);
+router.put("/:postId/comments/:commentId", updateComment);
+router.delete("/:postId/comments/:commentId", deleteComment);
 
 module.exports = router;
